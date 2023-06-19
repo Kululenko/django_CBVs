081e441cac9d7a8cdc582bf68f2e86f3f887dc30
Kululenko <alex.pmail@yahoo.com>
2023-06-19 16:31:03 +0200

    added: templates, views, urlpatters(regex), admin_registration, models, basic_app/templates, lowquality html/css, linking models to templates

--- basic_app/views.py
+++ basic_app/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import View, TemplateView,ListView,DetailView
+from . import models
 
 # Create your views here.
-
 class IndexView(TemplateView):
     template_name = 'index.html'
 
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['injectme'] = 'BASIC INJECTION'
-        return context
+class SchoolListView(ListView):
+    context_object_name = 'schools'     #change name from school_list to schools for clarification
+    model = models.School               # lists all school objects / way simplier and cleaner then function 
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
